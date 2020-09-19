a3c48fd876d53cf6f9c7a94ea5d4b1e8bfcf93a7
Анисимова Анастасия Антоновна <AAA.18@free.uni-dubna.ru>
2020-09-19 12:01:48 +0300

    comm

--- les2/Sem2.md
+++ les2/Sem2.md
@@ -2,3 +2,7 @@ offset с какого номера начинается лимит
 npm пакет менеджера включающий библиотеки 
 npm inistall - для всех зависимостей 
 quokka start a current file
+
+
+коммент ctrl k c 
+раскомментить  ctrl k u
--- les2/npm in/index.js
+++ les2/npm in/index.js
@@ -3,15 +3,45 @@ require('dotenv').config() //load .env to procces .env
 const {Client} = require('pg')
 const client = new Client({
 
-    host: '95.217.232.188',
-    port: 7777,
-    user: "anisimova",
-    password: "123123123",
-    database: "anisimova"
+    host:process.env.DB_HOST, //'95.217.232.188',
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password:process.env.DB_PASSWORD,
+    database:process.env.DB_DATABASE
 })
 
+const name = 'name1'
+
 client.connect()
-client.query('SELECT NOW()', (err,res) => {
-    console.log(err,res)
-    client.end()
+client
+  .query(`  SELECT *
+           From test
+            WHERE name = $1
+            `, [name])
+  .then(result => console.log(result))
+  .catch(e => console.error(e.stack))
+  .then(() => client.end())
+
+
+// client.connect()
+// client.query(`
+//             SELECT *
+//             From test
+//             WHERE name = '${name}' ` // уязвим к sql иньекциям
+//              , (err,res) => { //=> THE SAME AS FUNCTION
+//     console.log(err,res)
+//     client.end()
+// })
+
+/*
+параметрезованный запрос
+const { Client } = require('pg')
+const client = new Client()
+client.connect()
+client.query('SELECT $1::text as name', ['brianc'], (err, res) => {
+  if (err) throw err
+  console.log(res)
+  client.end()
 })
+
+*/
