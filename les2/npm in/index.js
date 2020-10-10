16504d05ad7b5b5a43d6c75e7b10fda9d29b83c1
Anastasiia <aaa.18@uni-dubna.ru>
2020-10-10 14:01:19 +0300

    comm

--- les2/npm in/index.js
+++ les2/npm in/index.js
@@ -1,47 +1,84 @@
-//точка входа
-require('dotenv').config() //load .env to procces .env
-const {Client} = require('pg')
+require('dotenv').config()
+const { Client } = require('pg')
 const client = new Client({
-
-    host:process.env.DB_HOST, //'95.217.232.188',
+    host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     user: process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_DATABASE
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
 })
 
-const name = 'name1'
+const name = '1 or 1 = 1'
 
 client.connect()
-client
-  .query(`  SELECT *
-           From test
-            WHERE name = $1
-            `, [name])
-  .then(result => console.log(result))
-  .catch(e => console.error(e.stack))
-  .then(() => client.end())
+async function addCar() {
+    const car = {
+        brandID: 1,
+        model: '420i',
+        cost: 25000000,
+        year: 2019,
+        isAvaible: true,
+    }
+    try {
+        // start transction
+        await client.query('BEGIN')
+        const res = await client.query(
+            `
+        INSERT INTO car (brand_id,model,cost,year_of_creation,is_avaible) VALUES
+        ($1,$2,$3,$4,$5) RETURNING id
+    `,
+            [car.brandID, car.model, car.cost, car.year, car.isAvaible]
+        )
 
+        const carID = res.rows[0].id
+        const resManager = await client.query(`
+    SELECT *
+    FROM manager
+    WHERE car_id IS NULL
+    LIMIT 1
+    `)
+        const managerID = resManager.rows[0].id
+
+        await client.query(
+            `
+        UPDATE manager 
+        SET car_id = $1
+        WHERE id = $2
+    `,
+            [carID, managerID]
+        )
+        await client.query('COMMIT')
+    } catch (err) {
+        client.query('ROLLBACK')
+        throw err
+    } finally {
+        client.end()
+    }
+}
+addCar()
+// client
+//     .query('SELECT NOW()')
+//     .then((result) => console.log(result))
+//     .catch((e) => console.error(e.stack))
+//     .then(() => client.end())
+
+// 1 по указанному id менять статус на false
+// 2 по указанному id снижать цену на 10 %
+
+// 1 получить все id машин старше 2018
+// 2 снизить цену на полученные авто на 5%
+
+// 1 добавить новый автомобиль
+// 2 получить одного свободного менеджера
+// 3 назначить ему для продажи новый автомобиль
 
 // client.connect()
 // client.query(`
-//             SELECT *
-//             From test
-//             WHERE name = '${name}' ` // уязвим к sql иньекциям
-//              , (err,res) => { //=> THE SAME AS FUNCTION
+//     SELECT *
+//     FROM student
+//     WHERE name = $1
+//     `,[name],function(err,res){
 //     console.log(err,res)
 //     client.end()
 // })
-
-/*
-параметрезованный запрос
-const { Client } = require('pg')
-const client = new Client()
-client.connect()
-client.query('SELECT $1::text as name', ['brianc'], (err, res) => {
-  if (err) throw err
-  console.log(res)
-  client.end()
-})
-
-*/
+console.log(1)
